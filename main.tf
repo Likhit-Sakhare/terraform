a826c02a430d1533f551c79d03cc3e1a25b6623f
Likhit Sakhare <sakhare1181likhit@gmail.com>
2025-12-09 16:35:42 +0530

    Creating the instance from module

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ module "vpc" {
     vpc_pub_subnet_cidr_block = "10.0.1.0/24"
     vpc_pri_subnet_cidr_block = "10.0.2.0/24"
 }
+
+module "instance" {
+    source = "./modules/instance"
+
+    project = "likhit-module"
+    ami = "ami-0fa91bc90632c73c9"
+    instance_type = "t3.micro"
+}
